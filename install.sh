76bfe8467a6572cda33006a00b2b31c4a321704a
FII14 <121723975+FII14@users.noreply.github.com>
2023-04-22 13:56:50 +0700

    Update and rename istall.sh to install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@
 # ------- pembuat: fii14 --------
 #--------------------------------
 
+h="\e[1;32m"
+p="\e[0m"
+m="\e[1;31m"
+
 while true
 do
   
@@ -17,14 +21,17 @@ do
     unzip LOIC-1.0.8-binary.zip
     chmod +x LOIC.exe
     cp LOIC.exe /usr/bin
-    echo "LOIC berhasil diinstal."
+    echo -e "${h}[+] ${p}LOIC berhasil diinstal."
+    sleep 3
+    echo -e "${h}[+] ${p}Untuk menjalankannya ketikkan perintah LOIC.exe"
+    sleep 3
     exit 0
     
   elif [[ "${nanya}" == "tidak" ]]; then
     exit 0
   
   else
-    echo "Masukkan salah."
+    echo -e "${m}[-] ${p}Masukkan salah."
     continue
   fi
   
